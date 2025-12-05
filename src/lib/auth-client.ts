400c8914c2409fc34c78b844eed826ea9afeb8a2
Enriquefft <enriquefft2001@gmail.com>
2025-12-05 07:38:31 -0500

    feat: removed polar adn posthog

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,6 @@
-import { polarClient } from "@polar-sh/better-auth";
 import { createAuthClient } from "better-auth/react";
 
 export const { signIn, signUp, useSession, checkout, customer } =
-	createAuthClient({
-		plugins: [polarClient()],
-	});
+    createAuthClient({
+        plugins: [],
+    });
